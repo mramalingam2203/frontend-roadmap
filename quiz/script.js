f4b44450f223b0d4010e2e28eff6622c07399e2f
neonumerics <59771226+mramalingam2203@users.noreply.github.com>
2024-10-05 22:46:07 +0530

    script update

--- quiz/script.js
+++ quiz/script.js
@@ -1,34 +1,7 @@
 // script.js
-  // Function to handle navigation
-  function navigateToPage(url) {
-    window.location.href = url;
+ 
+
+
+function SameTab() {
+    window.location.replace("./quiz.html");
 }
-
-// Assign event listeners to each button
-document.getElementById("animal").addEventListener("click", function() {
-    navigateToPage("./animals.html");
-});
-
-document.getElementById("country").addEventListener("click", function() {
-    navigateToPage("country.html");
-});
-
-document.getElementById("ww2").addEventListener("click", function() {
-    navigateToPage("ww2.html");
-});
-
-document.getElementById("celebrity").addEventListener("click", function() {
-    navigateToPage("celebrity.html");
-});
-
-document.getElementById("music").addEventListener("click", function() {
-    navigateToPage("music.html");
-});
-
-document.getElementById("lit").addEventListener("click", function() {
-    navigateToPage("lit.html");
-});
-
-document.getElementById("tech").addEventListener("click", function() {
-    navigateToPage("tech.html");
-});
